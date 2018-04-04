8805b5cb4dd7e2df1c010b9aa359bbdbb65982b6
saritasakkad <akkadsaritas@gmail.com>
2018-04-04 14:51:58 +0200

    “ComponentsWithinComponents”

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ Vue.component('task', {
  
     template:'<li><slot></slot></li>'
 
-    // <slot></slot> permet d'aficher les meme task mais les diférent txte dans le meme task
-
+   
 });
 
 new Vue({
