bf490d942eb31b6cddbcb7514529fdc381088c3d
saritasakkad <akkadsaritas@gmail.com>
2018-04-09 11:14:50 +0200

    “Ex2EventDispatcherP1”

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-Vue.component('...', {
+window.Event = new Vue();
 
-   
+
+Vue.component('coupon', {
+
+    template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
+
+    methods: {
+
+        onCouponApplied() {
+
+            Event.$emit('applied');
+        }
+    }
 
 });
 
 new Vue({
     el: '#root',
 
-  
+    data: {
+
+
+        couponApplied: false
+        
+    },
+
+    created() {
+
+        Event.$on('applied', () => alert('frapakkad ip!'));
+    }
+    
 });
