d05a483b2d720b4a59ce19ec1d516341bbfcac7f
saritasakkad <akkadsaritas@gmail.com>
2018-04-09 10:21:16 +0200

    “ComponentCommunicationEx1CustomEventsOK”

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
-Vue.component('tabs', {
-    template: `
-      
+Vue.component('coupon', {
 
+    template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
 
-    
-    `
+    methods: {
+
+        onCouponApplied() {
+
+            this.$emit('applied')
+        }
+    }
+
 });
 
 new Vue({
-    el: '#root'
+    el: '#root',
+
+    data: {
+
+
+            couponApplied: false
+        
+    },
+
+    methods: {
+
+        onCouponApplied() {
+
+            this.couponApplied = true;
+        }
+    }
 });
