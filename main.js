7af43671d5e72bd577b1752a79f23df05bf7fc05
saritasakkad <akkadsaritas@gmail.com>
2018-04-09 11:29:06 +0200

    “NamedSlotsINaNutshell”

--- main.js
+++ main.js
@@ -1,53 +1,10 @@
-window.Event = new class {
 
-    constructor() {
+Vue.component('...', {
 
-        this.vue = new Vue();
-
-    }
-
-    fire(event, data = null) {
-
-        this.vue.$emit(event, data);
-
-
-    }
-
-    listen(event, callback) {
-        
-        this.vue.$on(event, callback);
-
-    }
-}
-
-
-Vue.component('coupon', {
-
-    template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
-
-    methods: {
-
-        onCouponApplied() {
-
-            Event.fire('applied');
-        }
-    }
 
 });
 
 new Vue({
     el: '#root',
 
-    data: {
-
-
-        couponApplied: false
-        
-    },
-
-    created() {
-
-        Event.listen('applied', () => alert('frapakkad ip!'));
-    }
-    
 });
