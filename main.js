9b68ecbbd76578cf64ab59a36a0f1fb83e8bcf2a
saritasakkad <akkadsaritas@gmail.com>
2018-04-09 10:24:17 +0200

    “ComponentCommunicationEx2EventDispatcher”

--- main.js
+++ main.js
@@ -1,32 +1,11 @@
-Vue.component('coupon', {
+Vue.component('...', {
 
-    template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
-
-    methods: {
-
-        onCouponApplied() {
-
-            this.$emit('applied')
-        }
-    }
+   
 
 });
 
 new Vue({
     el: '#root',
 
-    data: {
-
-
-            couponApplied: false
-        
-    },
-
-    methods: {
-
-        onCouponApplied() {
-
-            this.couponApplied = true;
-        }
-    }
+  
 });
