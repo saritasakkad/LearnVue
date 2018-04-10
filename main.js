b99f763e7115822a3e618784e4c9d5e791019847
saritasakkad <akkadsaritas@gmail.com>
2018-04-10 09:36:30 +0200

    “SingleUseComponentsAndInlineTemplates”

--- main.js
+++ main.js
@@ -1,33 +1,7 @@
-Vue.component('modal', {
+Vue.component('....', {
     template: `
       
-    <div class="modal is-active">
-     <div class="modal-background"></div>
-      <div class="modal-card">
 
-        <header class="modal-card-head">
-            <p class="modal-card-title">
-            
-                <slot name="header"></slot>
-            
-            </p>
-            <button class="delete" aria-label="close"></button>
-        </header>
-
-        <section class="modal-card-body">
-
-            <slot></slot>
-
-        </section>
-
-        <footer class="modal-card-foot">
-
-            <slot name="footer"></slot>
-
-        </footer>
-
-      </div>
-    </div>
 
 
     `
