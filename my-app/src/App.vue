fba07d62dc9322df5935098e2b3c3f056d86e909
saritasakkad <akkadsaritas@gmail.com>
2018-04-11 10:17:48 +0200

    “NpmProjetOKOKOK”

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+
+ <div id="app">
+
+   <message>Akkad the Boss</message>
+
+   <message>Akkad the Boss toujour lui !</message>
+
+ </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+
+import Message from './components/Message.vue';
 
 export default {
+
   name: 'App',
-  components: {
-    HelloWorld
+
+  components: { Message },
+
+  data () {
+
+    return {
+
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
