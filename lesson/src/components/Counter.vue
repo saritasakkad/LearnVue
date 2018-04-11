801db186af4af9a8b19de22be42217be6ff77ddf
saritasakkad <akkadsaritas@gmail.com>
2018-04-11 11:30:27 +0200

    “AjoutComponentEXHMROKOKOK”

---
+++ lesson/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  
+    <div>
+
+        <h2>Akkad the: {{ Boss }}</h2>
+
+        <p>
+
+            <button @click="Boss += 1">Big</button>
+
+        </p>
+
+    </div>
+
+</template>
+
+
+<script>
+    
+    export default {
+
+        data() {
+            return { Boss: 0 };
+        }
+    }
+
+</script>
